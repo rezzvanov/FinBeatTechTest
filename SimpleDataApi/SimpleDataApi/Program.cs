c175c86806bbc13aa09c3ec999bf3a1f2fa26dac
rezzvanov <55750175+rezzvanov@users.noreply.github.com>
2025-03-27 21:29:01 +0300

    don't use top-level statement

--- SimpleDataApi/SimpleDataApi/Program.cs
+++ SimpleDataApi/SimpleDataApi/Program.cs
@@ -2,34 +2,40 @@ using Microsoft.EntityFrameworkCore;
 using SimpleDataApi.Services;
 using SimpleDataApi.Storage;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-               ?? throw new InvalidOperationException("Connection string not found");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddScoped<ICodeValuesService, CodeValuesService>();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? throw new InvalidOperationException("Connection string not found");
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        builder.Services.AddScoped<ICodeValuesService, CodeValuesService>();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
